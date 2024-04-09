b2c31676c3508a1e75b92cf3e382e8ef6e42a941
Makoena20 <koenaletseku@gmail.com>
2024-04-09 02:36:11 -0700

    1-binary_tree_insert_left.c

--- binary_trees.h
+++ binary_trees.h
@@ -24,5 +24,8 @@ typedef struct binary_tree_s
 /* Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
 #endif /* BINARY_TREES_H */
 
