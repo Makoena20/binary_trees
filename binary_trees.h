751771813091a7f1db6d8cd10e996704d236e00c
Makoena20 <koenaletseku@gmail.com>
2024-04-09 06:02:23 -0700

    16-binary_tree_is_perfect.c

--- binary_trees.h
+++ binary_trees.h
@@ -40,5 +40,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
+int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
+
 #endif /* BINARY_TREES_H */
 
