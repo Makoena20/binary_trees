f05816a832d764e865f590c96ec2c931b1ba3dac
Makoena20 <koenaletseku@gmail.com>
2024-04-10 05:26:27 -0700

    8-binary_tree_postorder.c

--- binary_trees.h
+++ binary_trees.h
@@ -66,5 +66,9 @@ void binary_tree_print(const binary_tree_t *);
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
+
 #endif /* BINARY_TREES_H */
 
