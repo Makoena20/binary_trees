7edc6b50812bab5985cb6e68e7e8ca508d75044e
Makoena20 <koenaletseku@gmail.com>
2024-04-12 02:49:11 -0700

    103-binary_tree_rotate_left.c

--- binary_trees.h
+++ binary_trees.h
@@ -85,5 +85,7 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+
 #endif /* BINARY_TREES_H */
 
