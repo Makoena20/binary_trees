d0814d8367cb385775b3f96f5a21fb3927de475b
Makoena20 <koenaletseku@gmail.com>
2024-04-15 06:36:07 -0700

    112-array_to_bst.c

--- binary_trees.h
+++ binary_trees.h
@@ -112,16 +112,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 bst_t *bst_insert(bst_t **tree, int value);
 void binary_tree_print(const binary_tree_t *);
 
-/* Binary search tree node structure */
-typedef struct binary_tree_s bst_t;
-
-/* Binary tree print function */
-void binary_tree_print(const binary_tree_t *);
-
-/* Function prototypes */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-bst_t *array_to_bst(int *array, size_t size);
-void bst_insert(bst_t **tree, int value);
 
 #endif /* BINARY_TREES_H */
 
