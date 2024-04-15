8eedd539bf657eae5b70b5e6b7c6a63601dfd2b1
Makoena20 <koenaletseku@gmail.com>
2024-04-14 23:26:59 -0700

    102-binary_tree_is_complete.c

--- binary_trees.h
+++ binary_trees.h
@@ -87,6 +87,11 @@ size_t binary_tree_size(const binary_tree_t *tree);
 
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+void binary_tree_print(const binary_tree_t *tree);
+
+int binary_tree_is_complete(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
 
