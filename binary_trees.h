1e1362f93569b85e4ae3d4a6af7e623180f06806
Makoena20 <koenaletseku@gmail.com>
2024-04-15 07:05:38 -0700

    114-bst_remove.c

--- binary_trees.h
+++ binary_trees.h
@@ -118,9 +118,6 @@ void binary_tree_print(const binary_tree_t *);
 
 bst_t *bst_search(const bst_t *tree, int value);
 
-bst_t *bst_insert(bst_t **tree, int value);
-bst_t *array_to_bst(int *array, size_t size);
-void binary_tree_print(const binary_tree_t *);
 
 #endif /* BINARY_TREES_H */
 
