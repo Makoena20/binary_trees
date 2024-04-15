14b6b407ba326fe48d20841dbe837062909bf8a5
Makoena20 <koenaletseku@gmail.com>
2024-04-15 06:03:46 -0700

    110-binary_tree_is_bst.c

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
+#include <limits.h>
 
 /**
- * is_bst_util - Utility function to check if a binary tree is a valid BST
- * @tree: A pointer to the root node of the tree
- * @min: The minimum value allowed for nodes in this subtree
- * @max: The maximum value allowed for nodes in this subtree
- *
- * Return: 1 if the tree is a valid BST, otherwise 0
+ * is_bst_util - Helper function to check if a binary tree is a valid BST
+ * @tree: Pointer to the root node of the tree
+ * @min: Minimum allowed value for a node
+ * @max: Maximum allowed value for a node
+ * Return: 1 if tree is a valid BST, 0 otherwise
  */
 int is_bst_util(const binary_tree_t *tree, int min, int max)
 {
@@ -22,9 +22,8 @@ int is_bst_util(const binary_tree_t *tree, int min, int max)
 
 /**
  * binary_tree_is_bst - Checks if a binary tree is a valid Binary Search Tree
- * @tree: A pointer to the root node of the tree to check
- *
- * Return: 1 if tree is a valid BST, 0 otherwise
+ * @tree: Pointer to the root node of the tree to check
+ * Return: 1 if tree is a valid BST, and 0 otherwise
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
--- binary_trees.h
+++ binary_trees.h
@@ -104,5 +104,6 @@ int binary_tree_is_bst(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
 
+
 #endif /* BINARY_TREES_H */
 
